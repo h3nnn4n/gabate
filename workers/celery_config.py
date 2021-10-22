79e395f45b3470e15ad987978d83792f794bcc8c
h3nnn4n <uber.renan@gmail.com>
2021-10-22 19:41:11 -0300

    Tune celery (prefetch and acks late)

--- workers/celery_config.py
+++ workers/celery_config.py
@@ -8,6 +8,9 @@ load_dotenv()
 broker_url = os.environ.get("BROKER_URL", "pyamqp://guest@localhost")
 result_backend = os.environ.get("RESULT_BACKEND", "redis://localhost")
 
+task_acks_late = True
+worker_prefetch_multiplier = 1
+
 task_serializer = "json"
 result_serializer = "json"
 accept_content = ["json"]
