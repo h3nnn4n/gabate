110d14b79e3c5410017fc000d9c5009aae543f69
h3nnn4n <uber.renan@gmail.com>
2021-10-22 16:43:02 -0300

    Run 50 random agents and store the best

--- workers/main.py
+++ workers/main.py
@@ -1,11 +1,15 @@
+import json
 import sys
+from copy import copy
 from random import random
 
 import tasks
 
 
 def main(fast):
+    best = {"lines_cleared": 0}
     results = []
+
     agent_settings = {
         "agent": {"weights": [(random() * 2 - 1) * 5 for _ in range(14 * 3)]}
     }
@@ -14,15 +18,17 @@ def main(fast):
         print(tasks.run_agent_task(agent_settings))
         return
 
-    for i in range(5):
+    for i in range(50):
         agent_settings["agent"]["weights"] = [
             (random() * 2 - 1) * 5 for _ in range(14 * 3)
         ]
-        results.append(tasks.a_plus_b.delay(i, i + 1))
         results.append(tasks.run_agent_task.delay(agent_settings))
 
-    for result in results:
-        print(result.get())
+    for index, result in enumerate(results):
+        data = json.loads(result.get())
+        if data["lines_cleared"] > best["lines_cleared"]:
+            best = copy(data)
+            print(f'{index:4} {best["lines_cleared"]:4}')
 
 
 if __name__ == "__main__":
