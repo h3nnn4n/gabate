9d4ec9a1360702e9a4c3b5afedc2eaea1b3ffa77
h3nnn4n <uber.renan@gmail.com>
2021-10-22 22:54:01 -0300

    Fix test

--- trainer/tests/test_agent.py
+++ trainer/tests/test_agent.py
@@ -1,12 +1,9 @@
-import json
-
 from ..agent import Agent
 
 
 def test_to_json():
     agent = Agent()
-    raw_json = agent.to_json()
-    data = json.loads(raw_json)
+    data = agent.get_agent_data()
 
     for weight in data["agent"]["weights"]:
         assert -5 <= weight and weight <= 5
