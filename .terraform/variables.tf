281beb8fcc5a6178da2af3722aedc4462bc22f2f
h3nnn4n <uber.renan@gmail.com>
2021-10-30 09:17:42 -0300

    Remove more aws stuff

--- .terraform/variables.tf
+++ .terraform/variables.tf
@@ -38,20 +38,6 @@ variable "ip_range" {
 
 # Aws stuff
 
-variable "aws_namespace" {
-  default = "gabate"
-}
-
-variable "aws_vpc_cidr_block" {
-  description = "The top-level CIDR block for the VPC."
-  default     = "10.1.0.0/16"
-}
-
-variable "aws_cidr_blocks" {
-  description = "The CIDR blocks to create the workstations in."
-  default     =  ["10.1.1.0/24", "10.1.2.0/24"]
-}
-
 variable "aws_region" {
   default = "us-east-1"
 }
