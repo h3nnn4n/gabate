65d07b7fd907c86a043cfbe322fc4f571dfb946f
h3nnn4n <uber.renan@gmail.com>
2021-10-31 19:26:33 -0300

    Use domain to connect to broker and result backend

--- .terraform/worker_user_data/setup_worker_env.sh
+++ .terraform/worker_user_data/setup_worker_env.sh
@@ -4,8 +4,8 @@ cd /home/devops/
 
 # Setup worker secrets
 touch .env
-echo "BROKER_URL=pyamqp://$(cat /opt/rabbitmq_user):$(cat /opt/rabbitmq_passwd)@$(cat /opt/db_server_ip)" >> .env
-echo "RESULT_BACKEND=redis://$(cat /opt/redis_user):$(cat /opt/redis_passwd)@$(cat /opt/db_server_ip)" >> .env
+echo "BROKER_URL=pyamqp://$(cat /opt/rabbitmq_user):$(cat /opt/rabbitmq_passwd)@gabate.h3nnn4n.me" >> .env
+echo "RESULT_BACKEND=redis://$(cat /opt/redis_user):$(cat /opt/redis_passwd)@gabate.h3nnn4n.me" >> .env
 
 # Setup aws credentials
 mkdir .aws
