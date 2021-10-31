ba0cf2edc9a8c40c5e9b96f6812e7ce2e3c87958
h3nnn4n <uber.renan@gmail.com>
2021-10-31 10:44:00 -0300

    Dont reevaluate agents that didnt change + clone helper + tests

--- trainer/agent.py
+++ trainer/agent.py
@@ -19,24 +19,39 @@ class Agent:
 
         self.pending_results = []
 
+        self._scores = []
+        self._dirty_fitness = True
+
     def set_random_weights(self):
+        self._dirty_fitness = True
         self.settings["weights"] = [uniform(-5.0, 5.0) for _ in range(self.n_weights)]
 
     def set_weights(self, weights):
-        assert len(weights) == len(self.weights)
-        self.weights = copy(weights)
+        self._dirty_fitness = True
+        assert len(weights) == len(self.settings["weights"])
+        self.settings["weights"] = copy(self.settings["weights"])
 
     def get_agent_data(self):
         return {"agent": self.settings}
 
-    def trigger_eval(self):
+    def trigger_eval(self, force=False):
+        if not force and not self._dirty_fitness:
+            return
+
         data = self.get_agent_data()
 
         self.pending_results = [worker.send_task("tasks.evaluate_agent", args=[data]) for _ in range(self.n_evals)]
 
-    def get_fitness(self):
+    def _get_scores(self):
         values = [json.loads(result.get()) for result in self.pending_results]
-        scores = [value.get("lines_cleared") for value in values]
+        return [value.get("lines_cleared") for value in values]
+
+    def get_fitness(self):
+        if self._dirty_fitness:
+            self._dirty_fitness = False
+            self._scores = self._get_scores()
+
+        scores = self._scores
 
         fitness = {
             "min": min(scores),
@@ -46,3 +61,10 @@ class Agent:
         }
 
         return fitness
+
+    def clone(self):
+        new = Agent()
+        new.settings["weights"] = copy(self.settings["weights"])
+        new._dirty_fitness = self._dirty_fitness
+        new._scores = copy(self._scores)
+        return new
--- trainer/tests/test_agent.py
+++ trainer/tests/test_agent.py
@@ -1,4 +1,5 @@
 from ..agent import Agent
+from mock import patch
 
 
 def test_to_json():
@@ -7,3 +8,46 @@ def test_to_json():
 
     for weight in data["agent"]["weights"]:
         assert -5 <= weight and weight <= 5
+
+
+def test_clone_weights():
+    agent = Agent()
+    new_agent = agent.clone()
+
+    assert len(agent.settings["weights"]) == len(new_agent.settings["weights"])
+    assert agent.settings["weights"] == new_agent.settings["weights"]
+
+
+def test_clone_dirty():
+    agent = Agent()
+    new_agent = agent.clone()
+
+    assert agent._dirty_fitness == new_agent._dirty_fitness
+    assert new_agent._dirty_fitness
+
+
+def test_clone_non_dirty():
+    agent = Agent()
+    with patch.object(agent, "_get_scores", return_value=[1, 2, 3]):
+        agent.get_fitness()
+
+    new_agent = agent.clone()
+
+    assert agent._dirty_fitness == new_agent._dirty_fitness
+    assert not new_agent._dirty_fitness
+
+
+def test_fitness():
+    agent = Agent()
+    with patch.object(agent, "_get_scores", return_value=[1, 2, 3]):
+        scores = agent.get_fitness()
+
+    assert "min" in scores.keys()
+    assert "median" in scores.keys()
+    assert "max" in scores.keys()
+    assert "raw" in scores.keys()
+
+    assert scores["min"] == 1
+    assert scores["median"] == 2
+    assert scores["max"] == 3
+    assert scores["raw"] == [1, 2, 3]
