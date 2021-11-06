050a96a9769b645de30d763e0614f337de5bfc25
h3nnn4n <uber.renan@gmail.com>
2021-11-06 17:09:01 -0300

    Set security groups + t2 -> t3 micro

--- .terraform/redis.tf
+++ .terraform/redis.tf
@@ -2,12 +2,12 @@
 resource "aws_elasticache_cluster" "example" {
   cluster_id           = "${var.name}-result-backend"
   engine               = "redis"
-  node_type            = "cache.t2.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis6.x"
   engine_version       = "6.x"
   port                 = 6379
-  security_groups      = [aws_security_group.message-broker.id]
+  security_group_ids   = [aws_security_group.result-backend.id]
   apply_immediately    = true
   availability_zone    = var.availability_zones[0]
 }
