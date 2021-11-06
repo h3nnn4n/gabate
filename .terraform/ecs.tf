6722d8f78bc53eeeca4054e6b8f73f3de23244e5
h3nnn4n <uber.renan@gmail.com>
2021-11-06 18:03:35 -0300

    Run only one task for now

--- .terraform/ecs.tf
+++ .terraform/ecs.tf
@@ -39,7 +39,7 @@ resource "aws_ecs_service" "main" {
  name                               = "${var.name}-service"
  cluster                            = aws_ecs_cluster.main.id
  task_definition                    = aws_ecs_task_definition.main.arn
- desired_count                      = 4
+ desired_count                      = 1
  deployment_minimum_healthy_percent = 50
  deployment_maximum_percent         = 200
  launch_type                        = "FARGATE"
