450f80e6c54474806e3f26d21cb68067e7fc2a21
h3nnn4n <uber.renan@gmail.com>
2021-11-09 07:55:49 -0300

    Add redis service

--- .terraform/ecs.tf
+++ .terraform/ecs.tf
@@ -1,3 +1,4 @@
+# Cluster
 resource "aws_ecs_cluster" "main" {
   name = "${var.name}-cluster"
 
@@ -7,6 +8,50 @@ resource "aws_ecs_cluster" "main" {
   }
 }
 
+# Service definitions
+resource "aws_ecs_service" "main" {
+ name                               = "${var.name}-worker"
+ cluster                            = aws_ecs_cluster.main.id
+ task_definition                    = aws_ecs_task_definition.main.arn
+ desired_count                      = 1
+ deployment_minimum_healthy_percent = 50
+ deployment_maximum_percent         = 200
+ launch_type                        = "FARGATE"
+ scheduling_strategy                = "REPLICA"
+
+ network_configuration {
+   security_groups  = [aws_security_group.ecs_tasks.id]
+   subnets          = aws_subnet.private.*.id
+   assign_public_ip = false
+ }
+
+ lifecycle {
+   ignore_changes = [task_definition, desired_count]
+ }
+}
+
+resource "aws_ecs_service" "redis" {
+ name                               = "${var.name}-redis"
+ cluster                            = aws_ecs_cluster.main.id
+ task_definition                    = aws_ecs_task_definition.redis.arn
+ desired_count                      = 1
+ deployment_minimum_healthy_percent = 50
+ deployment_maximum_percent         = 200
+ launch_type                        = "FARGATE"
+ scheduling_strategy                = "REPLICA"
+
+ network_configuration {
+   security_groups  = [aws_security_group.ecs_tasks.id, aws_security_group.redis.id]
+   subnets          = aws_subnet.private.*.id
+   assign_public_ip = false
+ }
+
+ lifecycle {
+   ignore_changes = [task_definition, desired_count]
+ }
+}
+
+# Task definitions
 resource "aws_ecs_task_definition" "main" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -44,23 +89,35 @@ resource "aws_ecs_task_definition" "main" {
   ])
 }
 
-resource "aws_ecs_service" "main" {
- name                               = "${var.name}-worker"
- cluster                            = aws_ecs_cluster.main.id
- task_definition                    = aws_ecs_task_definition.main.arn
- desired_count                      = 1
- deployment_minimum_healthy_percent = 50
- deployment_maximum_percent         = 200
- launch_type                        = "FARGATE"
- scheduling_strategy                = "REPLICA"
-
- network_configuration {
-   security_groups  = [aws_security_group.ecs_tasks.id]
-   subnets          = aws_subnet.private.*.id
-   assign_public_ip = false
- }
-
- lifecycle {
-   ignore_changes = [task_definition, desired_count]
- }
+resource "aws_ecs_task_definition" "redis" {
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  family                   = "${var.name}-redis"
+  container_definitions = jsonencode([
+    {
+      name      = "${var.name}-redis"
+      image     = "redis:6.2"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      environment = [
+        {
+          name = "foo",
+          value = "bar"
+        },
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-stream-prefix = "ecs"
+          awslogs-group         = aws_cloudwatch_log_group.main.name
+          awslogs-region        = var.aws_region
+        }
+      }
+    }
+  ])
 }
