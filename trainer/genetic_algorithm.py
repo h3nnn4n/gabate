08e6faddc5292d4053aba7bf61e042513f802feb
h3nnn4n <uber.renan@gmail.com>
2023-12-07 07:50:48 -0300

    Move genetic algorithm code to a matching file

--- trainer/genetic_algorithm.py
+++ trainer/genetic_algorithm.py
@@ -9,6 +9,32 @@ import utils
 from agent import Agent
 
 
+def genetic_algorithm():
+    print("starting genetic algorithm")
+    print("settings:")
+    print(f"   FITNESS_MODE: {config.FITNESS_MODE}")
+    print(f"  N_GENERATIONS: {config.N_GENERATIONS}")
+    print(f"POPULATION_SIZE: {config.POPULATION_SIZE}")
+    print(f"  N_AGENT_EVALS: {config.N_AGENT_EVALS}")
+    print(f" CROSSOVER_RATE: {config.CROSSOVER_RATE}")
+    print(f"  MUTATION_RATE: {config.MUTATION_RATE}")
+    print()
+    print("starting...")
+
+    population = Population()
+
+    for _ in range(config.N_GENERATIONS):
+        population.start_generation()
+        population.evaluate_population_fitness()
+        population.store_elite_individual()
+        population.selection()
+        population.crossover()
+        population.mutation()
+        population.end_generation()
+
+    print("finished")
+
+
 class Individual:
     def __init__(self, agent=None):
         self._agent = agent or Agent()
--- trainer/main.py
+++ trainer/main.py
@@ -1,30 +1,8 @@
-import config
-from population import Population
+from genetic_algorithm import genetic_algorithm
 
 
 def main():
-    print("settings")
-    print(f"   FITNESS_MODE: {config.FITNESS_MODE}")
-    print(f"  N_GENERATIONS: {config.N_GENERATIONS}")
-    print(f"POPULATION_SIZE: {config.POPULATION_SIZE}")
-    print(f"  N_AGENT_EVALS: {config.N_AGENT_EVALS}")
-    print(f" CROSSOVER_RATE: {config.CROSSOVER_RATE}")
-    print(f"  MUTATION_RATE: {config.MUTATION_RATE}")
-    print()
-    print("starting...")
-
-    population = Population()
-
-    for generation in range(config.N_GENERATIONS):
-        population.start_generation()
-        population.evaluate_population_fitness()
-        population.store_elite_individual()
-        population.selection()
-        population.crossover()
-        population.mutation()
-        population.end_generation()
-
-    print("finished")
+    genetic_algorithm()
 
 
 if __name__ == "__main__":
