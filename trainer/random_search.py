8897e49f7df09c2fa9306c2da8ef020fcdb793d3
h3nnn4n <uber.renan@gmail.com>
2025-09-11 21:38:48 -0300

    Complete random search

--- trainer/random_search.py
+++ trainer/random_search.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import random, uniform
 
 import config
 from agent import Individual
@@ -13,30 +14,45 @@ class RandomSearch:
     def __init__(self):
         self.population_size = config.POPULATION_SIZE
         self.population = self.build_population()
+        self.elite_genes = self.population[0].genes
+        self.elite_score = 0
 
-        self.generations = 1
-        self.generation_time = datetime.now()
+        self.generation_count = 0
 
     def run(self):
+        for _ in range(config.N_GENERATIONS):
+            self.run_generation()
+
+    def run_generation(self):
+        t_start = datetime.now()
         for individual in self.population:
             individual.trigger_fitness_evaluation()
 
         scores = [individual.get_fitness() for individual in self.population]
 
-        generation_time = datetime.now()
-        generation_duration = (generation_time - self.generation_time).total_seconds()
+        generation_duration = (datetime.now() - t_start).total_seconds()
         diversity = self.get_diversity()
+        self.generation_count += 1
 
-        print(f"{self.generations:4d}/{config.N_GENERATIONS:4d}", end=" ", flush=True)
-        print(f"{min(scores):7d} {sum(scores) / len(scores):8.2f} {max(scores):7d}", end=" ")
-        print(f"{diversity:7.2f} {generation_duration:7.2f}", end="")
-        print()
+        self.elite = self.update_elite()
+
+        self.population = self.build_population()
+        self.population[0].genes = self.elite_genes
+        self.mutate_individual(self.population[0])
+
+        print(f"{self.generation_count:4d}/{config.N_GENERATIONS:4d}   ", end=" ")
+        print(f"min={min(scores):7d}   ", end=" ")
+        print(f"mean={sum(scores) / len(scores):8.2f}   ", end=" ")
+        print(f"max={max(scores):7d}   ", end=" ")
+        print(f"{diversity=:7.2f}   ", end=" ")
+        print(f"{generation_duration=:7.2f}   ", end=" ")
+        print(f"elite={self.elite_score=:7.2f}   ", end=" ")
+        print(flush=True)
 
     def build_population(self) -> list[Individual]:
         return [Individual() for _ in range(self.population_size)]
 
     def get_diversity(self) -> float:
-        # FIXME: Should be a config
         n_genes = self.population[0].n_genes
         value = 0
         count = 0
@@ -54,3 +70,18 @@ class RandomSearch:
                     value += abs(individual_a.genes[k] - individual_b.genes[k]) / n_genes
 
         return value / count
+
+    def update_elite(self) -> Individual:
+        new_elite = max(self.population, key=lambda x: x.get_fitness())
+        new_elite_score = new_elite.get_fitness()
+
+        if new_elite_score > self.elite_score:
+            self.elite_genes = new_elite.genes
+            self.elite_score = new_elite_score
+
+    def mutate_individual(self, individual: Individual):
+        for i in range(individual.n_genes):
+            if random() > config.MUTATION_RATE:
+                continue
+
+            individual.genes[i] *= uniform(0.9, 1.1)
