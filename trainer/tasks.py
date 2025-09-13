21518723ac51f85b45d07c7d1e6e344945c632c9
h3nnn4n <uber.renan@gmail.com>
2025-09-13 15:17:37 -0300

    Bump time list for tasks

--- trainer/tasks.py
+++ trainer/tasks.py
@@ -6,6 +6,6 @@ def a_plus_b(a, b):
     pass
 
 
-@dramatiq.actor(store_results=True)
+@dramatiq.actor(store_results=True, time_limit=1000 * 60 * 60 * 24 * 7)
 def evaluate_agent(agent_settings):
     pass
