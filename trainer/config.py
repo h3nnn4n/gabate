bb7666af62125b4d7da9205174c58b6a2f179321
h3nnn4n <uber.renan@gmail.com>
2025-09-14 13:50:44 -0300

    Type config

--- trainer/config.py
+++ trainer/config.py
@@ -7,17 +7,17 @@ load_dotenv()
 
 REDIS_URL: str = str(config("REDIS_URL", default="redis://localhost:6379"))
 
-N_GENERATIONS = config("N_GENERATIONS", default=100, cast=int)
-POPULATION_SIZE = config("POPULATION_SIZE", default=10, cast=int)
+N_GENERATIONS = int(config("N_GENERATIONS", default=100, cast=int))
+POPULATION_SIZE = int(config("POPULATION_SIZE", default=10, cast=int))
 
-N_AGENT_EVALS = config("N_AGENT_EVALS", default=5, cast=int)
+N_AGENT_EVALS = int(config("N_AGENT_EVALS", default=5, cast=int))
 
-CROSSOVER_RATE = config("CROSSOVER_RATE", default=0.3, cast=float)
-MUTATION_RATE = config("MUTATION_RATE", default=0.1, cast=float)
+CROSSOVER_RATE = float(config("CROSSOVER_RATE", default=0.3, cast=float))
+MUTATION_RATE = float(config("MUTATION_RATE", default=0.1, cast=float))
 
-ELITE_MUTATIONS = config("ELITE_MUTATIONS", default=5, cast=int)
+ELITE_MUTATIONS = int(config("ELITE_MUTATIONS", default=5, cast=int))
 
 # Can be "max", "min", "avg", "sum" or "median"
-FITNESS_MODE = config("FITNESS_MODE", default="max", cast=str)
+FITNESS_MODE = str(config("FITNESS_MODE", default="max", cast=str))
 
-FEATURE_SET_NAME = config("FEATURE_SET_NAME", default="LELmark", cast=str)
+FEATURE_SET_NAME = str(config("FEATURE_SET_NAME", default="LELmark", cast=str))
