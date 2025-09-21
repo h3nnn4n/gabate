c3f5ca5d866719da79a3c0c1c556fbb5670368b9
h3nnn4n <uber.renan@gmail.com>
2025-09-21 12:13:23 -0300

    Make printing updates not dependent on main loop sleep

--- trainer/local_search.py
+++ trainer/local_search.py
@@ -12,6 +12,8 @@ logger = logging.getLogger(__name__)
 STEP_SIZE = config.GENE_STEP_SIZE  # type: ignore
 DURATION = timedelta(minutes=5)
 
+UPDATE_INTERVAL = timedelta(seconds=10)
+
 
 def local_search(individual_path: str):
     individual_name = individual_path.split("/")[-1]
@@ -35,26 +37,24 @@ def local_search(individual_path: str):
 
     evaluations = 0
     t_start = datetime.now()
+    last_update = datetime.now()
+    print_status(elite_individual, original_individual, evaluations, t_start)
+
     while datetime.now() - t_start < DURATION:
-        time_elapsed = datetime.now() - t_start
-        time_left = DURATION - time_elapsed
-        time_progress = time_elapsed.total_seconds() / DURATION.total_seconds()
-        gene_distance = sum(
-            (elite_individual.genes[i] - original_individual.genes[i]) ** 2 for i in range(elite_individual.n_genes)
-        ) ** 0.5
-        logger.info(
-            f"{evaluations=:6d}  distance={gene_distance:5.2f}  elite_score={elite_individual.get_fitness():8.2f}  elapsed={time_elapsed.total_seconds() / 60:5.2f}m left={time_left.total_seconds() / 60:5.2f}m ({time_progress:5.2%})"
-        )
+        if datetime.now() - last_update > UPDATE_INTERVAL:
+            print_status(elite_individual, original_individual, evaluations, t_start)
+            last_update = datetime.now()
 
         for individual in population:
             is_ready = individual.is_evaluation_ready
             if is_ready:
                 evaluations += 1
-                result = individual.get_fitness()
 
-                if result > elite_individual.get_fitness():
+                if individual.get_fitness() > elite_individual.get_fitness():
                     elite_individual = individual.clone()
-                    logger.debug(f"New elite individual: {json.dumps(result)}")
+                    print_status(elite_individual, original_individual, evaluations, t_start)
+
+                    logger.debug(f"New elite individual: {json.dumps(individual.get_raw_fitness())}")
                 else:
                     logger.debug(
                         f"Individual {individual.get_fitness()} isnt better than elite {elite_individual.get_fitness()}"
@@ -64,7 +64,7 @@ def local_search(individual_path: str):
                 mutate_individual(individual)
                 individual.trigger_fitness_evaluation()
 
-        sleep(5)
+        sleep(1)
 
     logger.info("Finished main loop. Waiting for remaining evaluations to finish")
     while not all(individual.is_evaluation_ready for individual in population):
@@ -79,7 +79,25 @@ def local_search(individual_path: str):
     logger.info(f"Elite individual: {json.dumps(elite_individual.get_raw_fitness())}")
 
 
-def mutate_individual(individual: Individual):
+def print_status(elite_individual: Individual, original_individual: Individual, evaluations: int, t_start: datetime) -> None:
+    time_elapsed = datetime.now() - t_start
+    time_left = DURATION - time_elapsed
+    time_progress = time_elapsed.total_seconds() / DURATION.total_seconds()
+    gene_distance = sum(
+        (elite_individual.genes[i] - original_individual.genes[i]) ** 2 for i in range(elite_individual.n_genes)
+    ) ** 0.5
+
+    evals_str = f"{evaluations=:6d}"
+    distance_str = f"distance={gene_distance:5.2f}"
+    elite_score_str = f"elite_score={elite_individual.get_fitness():8.2f}"
+    elapsed_str = f"elapsed={time_elapsed.total_seconds() / 60:5.2f}m"
+    left_str = f"left={time_left.total_seconds() / 60:5.2f}m"
+    progress_str = f"({time_progress:5.2%})"
+    tokens = [evals_str, distance_str, elite_score_str, elapsed_str, left_str, progress_str]
+    logger.info("   ".join(tokens))
+
+
+def mutate_individual(individual: Individual) -> None:
     genes = individual.genes
     random_gene_index = random.randint(0, individual.n_genes - 1)
     direction = random.choice([-1, 1])
